3a998f91f75f9f4a4da01362323e0470b0c87f58
RaptaG <77157639+RaptaG@users.noreply.github.com>
2022-06-21 15:56:32 +0300

    Improve and cleanup code
    - More human-readable names
    - Separate commands from the “main” command
    - General improvements

--- CAAIS.sh
+++ CAAIS.sh
@@ -1,28 +1,32 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# Made by RaptaG and me0wing-katt0, for your peace of mind :)
 
+# Crash Checker
 set -e
 
-VERSION='1.1'
+# Definitions
+ver='1.1'
+fname=$0
 
-eexit() {
-    echo 'Exiting...'
+caais-exit() {
+    echo "Exiting..."
     exit "$1"
 }
 
+# Starting message
+echo "CAAIS, version $ver (by RaptaG and me0wing-katt0)"
+
+# Root permission checker
+if [ "$EUID" -ne 0 ]; then
+     echo "Error: $fname requires root permissions. Please run: sudo ./$fname"
+     caais-exit 1
+fi
+
+# Main script
 main() {
-    # Made by RaptaG and me0wing-katt0, for your peace of mind :)
-    echo "CAAIS, version $VERSION (by RaptaG and me0wing-katt0)"
-
-    # Root permission checker
-    if [ "$EUID" -ne 0 ]; then
-        _bname="${0##*/}"
-
-        echo "$_bname requires root permissions. Please run: su -c './$_bname'" >&2
-        eexit 1
-    fi
 
     # Installing keys
-    echo 'Installing the Chaotic-AUR keys...'
+    echo "Installing the Chaotic-AUR keys..."
     pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
     pacman-key --lsign-key FBA220DFC880C036
 
@@ -34,36 +38,32 @@ main() {
     pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
     # Checking if Chaotic-AUR is already appended in pacman.conf, if not skip
-    echo 'Appending Chaotic-AUR to the mirrorlist...'
+    echo "Appending Chaotic-AUR to the mirrorlist..."
 
     if grep 'chaotic-aur' /etc/pacman.conf; then
-        echo 'Chaotic-AUR is already append in pacman.conf, skipping...'
-        eexit 2
+        echo "Chaotic-AUR is already append in pacman.conf, skipping..."
     else
-        echo -e "\r\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" >>/etc/pacman.conf
+        echo -e "\r\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
     fi
 
     # Refreshing the mirrorlist
-    echo 'Refreshing the mirrorlists...'
+    echo "Refreshing the mirrorlists..."
 
-    echo -n \
-        "doing this is needed for chaotic-aur to work,
-but will also cause a system upgrade. do you wish to continue?
-[y/N] "
-    read -r yn
+    echo -n "Doing this is needed for Chaotic-AUT to work, but will also cause a system upgrade. do you wish to continue? [Y/n] "
+    read answer
 
-    case "$yn" in
+    case "$answer" in
     [yY]*)
         pacman -Syu
         ;;
     *)
-        echo "You'll now have to refresh the mirrorlists manually, by running: su -c 'pacman -Syu'"
-        eexit
+        echo "You'll now have to refresh the mirrorlists manually, by running: sudo pacman -Syu"
+        caais-exit
         ;;
     esac
-
-    echo 'Done! Now Chaotic-AUR should be installed and working in your system!'
-    eexit
 }
 
+# Running the script
 main "$@"
+echo "Done! Now Chaotic-AUR should be installed and working in your system!"
+caais-exit
