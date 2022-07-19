5ed386051929d6d720f67eba834c6add0672a6fa
RaptaG <77157639+RaptaG@users.noreply.github.com>
2022-07-19 15:10:06 +0300

    Removal of `caais-exit`, more fixes
    Features missing: Removing Chaotic-AUR from pacman.conf
    After this added, v.1.7 will be released

--- CAAIS.sh
+++ CAAIS.sh
@@ -1,41 +1,36 @@
 #!/bin/bash
-# Made by RaptaG, terminalmaid and TruncatedDinosour for your peace of mind :)
+# Made by RaptaG, terminalmaid and TruncatedDinosour
 set -e
 
 # Definitions
-ver=1.7
-fname=$(basename $0)
+ver="1.7"
+fname="$(basename $0)"
 GHrelease="https://github.com/RaptaG/CAAIS/releases/tag/$ver"
 
-caais-exit() {
-    echo "Exiting..."
-    exit "$1"
-}
-
-# Starting message
+# Startup message
 echo "CAAIS, version $ver (by RaptaG, terminalmaid and TruncatedDinosour)"
 
 # Root permission checker
 if [ "$EUID" -ne 0 ]; then
      echo -e "Error: Root permissions are required for $fname to work.\nPlease run: sudo ./$fname"
-     caais-exit 1
+     exit 1
 fi
 
 # Download & install Chaotic-AUR
 caais-get() {
-    # Downloading & installing the Chaotic-AUR key
+    # Download & install the Chaotic-AUR key
     echo "Installing the Chaotic-AUR key..."
     pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com > /dev/null 2>&1
     pacman-key --lsign-key FBA220DFC880C036 > /dev/null 2>&1
 
-    # Downloading its keyring and mirrorlist
+    # Download its keyring and mirrorlist
     echo "Downloading its keyring..."
     pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm > /dev/null 2>&1
 
     echo "Downloading its mirrorlist..."
     pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm > /dev/null 2>&1
 
-    # Checking if it's already appended in pacman.conf, if not skip
+    # Check if it's already appended in pacman.conf, if not skip
     appendInPacmanConf=$(grep 'chaotic-aur' /etc/pacman.conf)
 
     if [ $appendInPacmanConf == "[chaotic-aur]" ]; then
@@ -45,40 +40,40 @@ caais-get() {
         echo -e "\r\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
     fi
 
-    # Refreshing the mirrorlists so that it can be activated
+    # Refresh the mirrorlists so that it can be activated
     echo "Refreshing the mirrorlists..."
     pacman -Sy
 
     # Finish message
     echo "Done! Now Chaotic-AUR should be installed and working in your system!"
-    caais-exit 0
+    exit 0
 }
 
 # Uninstall Chaotic-AUR
 caais-remove() {
-    # Deleting the Chaotic-AUR key
+    # Delete the Chaotic-AUR key
     echo "Deleting the Chaotic-AUR key..."
     pacman-key --delete 3FFA0B5E092ED4095E26F69B8ADB4AEC585061CF > /dev/null 2>&1
 
-    # Uninstalling its keyring
+    # Uninstall its keyring
     echo "Uninstalling its keyring..."
-    pacman -Rns chaotic-keyring > /dev/null 2>&1
+    pacman -Rns chaotic-keyring --no-confirm > /dev/null 2>&1
     
-    # Uninstalling its mirrorlist
+    # Uninstall its mirrorlist
     echo "Uninstalling the mirrorlist..."
-    pacman -Rns chaotic-mirrorlist > /dev/null 2>&1
+    pacman -Rns chaotic-mirrorlist --no-confirm > /dev/null 2>&1
 
-    # Removing Chaotic-AUR from pacman.conf
+    # Remove Chaotic-AUR from pacman.conf
     echo "Removing Chaotic-AUR from pacman.conf..."
     
 
-    # Refreshing the mirrorlists
+    # Refresh the mirrorlists
     echo "Refreshing the mirrorlists..."
     pacman -Sy
 
     # Finish message
     echo "Done! Now Chaotic-AUR should be removed from your system."
-    caais-exit 0
+    exit 0
 }
 
 # Select if you want to delete CAAIS
@@ -91,10 +86,10 @@ delete-caais() {
         echo "Deleting..."
         rm $fname
         echo -e "Remember, you can always reinstall CAAIS in case you want to remove Chaotic-AUR:\n$GHrelease"
-        caais-exit 0
+        exit 0
         ;;
     *)
-        caais-exit 0
+        exit 0
         ;;
     esac
 }
@@ -123,7 +118,7 @@ case "$1" in
     caais-remove "$@"
     ;;
 *)
-    echo "Invalid option. Please run 'sudo ./$fname --help' for more information."
-    caais-exit 1
+    echo "Invalid option '$1'. Please run 'sudo ./$fname --help' for more information."
+    exit 1
     ;;
 esac
